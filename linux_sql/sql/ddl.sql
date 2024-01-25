72db336c48d8558f34a4f1a6cae3fcf35eb2c3df
Tanya Nantsa <tanyanantsa@gmail.com>
2024-01-25 03:26:40 +0000

    implemented ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,40 +2,37 @@
 \c host_agent;
 
 -- Create `host_info` table if not exist
-CREATE TABLE IF NOT EXISTS PUBLIC.host_info
-    (
-        id SERIAL NOT NULL,
-        hostname VARCHAR NOT NULL,
-        cpu_number INT2 NOT NULL,
-        cpu_architecture VARCHAR NOT NULL,
-        cpu_model VARCHAR NOT NULL,
-        cpu_mhz FLOAT8 NOT NULL,
-        l2_cache INT4 NOT NULL,
-        "timestamp" TIMESTAMP NULL,
-        total_mem INT4 NULL,
-        CONSTRAINT host_info_pk PRIMARY KEY (id),
-        CONSTRAINT host_info_un UNIQUE (hostname)
-    );
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
+    id SERIAL NOT NULL,
+    hostname VARCHAR NOT NULL,
+    cpu_number INT2 NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model VARCHAR NOT NULL,
+    cpu_mhz FLOAT8 NOT NULL,
+    l2_cache INT4 NOT NULL,
+    "timestamp" TIMESTAMP NULL,
+    total_mem INT4 NULL,
+    CONSTRAINT host_info_pk PRIMARY KEY (id),
+    CONSTRAINT host_info_un UNIQUE (hostname)
+);
+
 -- Insert three sample data rows
 INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(1, 'jrvs-remote-desktop-centos7-6.us-central1-a.c.spry-framework-236416.internal', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
-
 INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(2, 'noe1', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
-
 INSERT INTO host_info (id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, "timestamp", total_mem) VALUES(3, 'noe2', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, '2019-05-29 17:49:53.000', 601324);
 
 -- Create `host_usage` table if not exist
-CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
-    (
-        "timestamp" TIMESTAMP NOT NULL,
-        host_id SERIAL NOT NULL,
-        memory_free INT4 NOT NULL,
-        cpu_idle INT2 NOT NULL,
-        cpu_kernel INT2 NOT NULL,
-        disk_io INT4 NOT NULL,
-        disk_available INT4 NOT NULL,
-        CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES
-        host_info(id)
-    );
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
+    "timestamp" TIMESTAMP NOT NULL,
+    host_id SERIAL NOT NULL,
+    memory_free INT4 NOT NULL,
+    cpu_idle INT2 NOT NULL,
+    cpu_kernel INT2 NOT NULL,
+    disk_io INT4 NOT NULL,
+    disk_available INT4 NOT NULL,
+    CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES
+    host_info(id)
+);
 
 -- Insert sample data
 INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
