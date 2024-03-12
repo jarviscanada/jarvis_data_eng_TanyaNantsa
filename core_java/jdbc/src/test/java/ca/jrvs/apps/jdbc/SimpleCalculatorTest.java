3469ecf26878c49cf3d7016c8066eefa3a133da4
Tanya Nantsa <tanyanantsa@gmail.com>
2024-03-12 21:49:46 +0000

    java testing practice files

---
+++ core_java/jdbc/src/test/java/ca/jrvs/apps/jdbc/SimpleCalculatorTest.java
@@ -0,0 +1,58 @@
+package ca.jrvs.apps.jdbc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleCalculatorTest {
+
+    SimpleCalculator calculator;
+
+    @BeforeEach
+    void init() {
+        calculator = new SimpleCalculatorImp();
+    }
+
+    @Test
+    void test_add() {
+        int expected = 2;
+        int actual = calculator.add(1, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_subtract() {
+        int expected = 7;
+        int actual = calculator.subtract(15, 8);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_multiply() {
+        int expected = 16;
+        int actual = calculator.multiply(4, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_divide() {
+        double expected = 5;
+        double actual = calculator.divide(50, 10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_power() {
+        double expected = 16;
+        double actual = calculator.power(4, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_abs() {
+        double expected = 6;
+        double actual = calculator.abs(-6);
+        assertEquals(expected, actual);
+    }
+}
