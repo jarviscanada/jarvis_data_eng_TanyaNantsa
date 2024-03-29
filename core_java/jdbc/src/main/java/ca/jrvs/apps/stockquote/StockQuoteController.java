975b07164aac010de96759bcc73ed2b783c98bcc
Tanya Nantsa <tanyanantsa@gmail.com>
2024-03-29 18:38:42 +0000

    added error and flow logging

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/StockQuoteController.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/StockQuoteController.java
@@ -1,24 +1,47 @@
 package ca.jrvs.apps.stockquote;
 
 import ca.jrvs.apps.stockquote.doa.Quote;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Optional;
 import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StockQuoteController {
 
     private QuoteService quoteService;
     private PositionService positionService;
+    private static final Logger flowLogger = LoggerFactory.getLogger("FlowLogger");
+    private static final Logger errorLogger = LoggerFactory.getLogger("ErrorLogger");
+    private static final String FLOW_LOG_FILE = "flow.log";
+    private static final String ERROR_LOG_FILE = "error.log";
 
     public StockQuoteController(QuoteService quoteService, PositionService positionService) {
         this.quoteService = quoteService;
         this.positionService = positionService;
     }
 
+    private void configureLogger(String logFile) {
+        try {
+            File file = new File(logFile);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.err.println("Error configuring logger: " + e.getMessage());
+        }
+    }
+
     /**
      * User interface for our application
      */
     public void initClient() {
+        configureLogger("flow.log");
+        configureLogger("error.log");
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -39,6 +62,7 @@ public class StockQuoteController {
 
                 switch (choice) {
                     case 1:
+                        flowLogger.info("Option 1 - Buy selected");
                         System.out.flush();
                         System.out.println("You selected Option 1 - Buy");
                         System.out.print("Enter ticket symbol you wish to buy: ");
@@ -46,56 +70,69 @@ public class StockQuoteController {
 
                         if (positionService.symbolInPosition(symbol)) {
                             System.out.println("Symbol already in portfolio - Try again");
+                            errorLogger.warn("Symbol already in portfolio - Try again");
                             break;
                         }
                         if (!quoteService.isValidSymbol(symbol)) {
                             System.out.println("Invalid ticket symbol - Try again");
+                            errorLogger.warn("Invalid ticket symbol - Try again");
                             break;
                         }
                         System.out.print("Enter number amount you wish to buy: ");
                         int num = scanner.nextInt();
                         if (!(num >= 0)) {
                             System.out.println("Number of shares must be a positive integer.");
+                            errorLogger.warn("Number of shares must be a positive integer.");
                             break;
                         }
                         Optional<Quote> optionalQuote = quoteService.fetchQuoteDataFromAPI(symbol);
                         if (optionalQuote.isPresent()) {
                             double price = optionalQuote.get().getPrice();
                             positionService.buy(symbol, num, price*num);
+                            flowLogger.info("Bought " + num + " shares of " + symbol + " at price " + price);
                         } else {
                             System.out.println("Invalid symbol - Try again");
+                            errorLogger.warn("Invalid symbol - Try again");
                         }
                         break;
                     case 2:
+                        flowLogger.info("Option 2 - Sell selected");
                         System.out.flush();
                         System.out.println("You selected Option 2 - Sell");
                         System.out.print("Enter ticket symbol you wish to sell: ");
                         symbol = scanner.nextLine();
                         if (!positionService.symbolInPosition(symbol)) {
                             System.out.println("Invalid ticket symbol - Try again");
+                            errorLogger.warn("Invalid ticket symbol - Try again");
                             break;
                         }
                         positionService.sell(symbol);
+                        flowLogger.info("Sold all shares of " + symbol);
                         break;
                     case 3:
+                        flowLogger.info("Option 3 - Search Symbol selected");
                         System.out.flush();
                         System.out.println("You selected Option 3 - Search Symbol");
                         System.out.print("Enter ticket symbol you wish to look up: ");
                         symbol = scanner.nextLine();
                         if (!quoteService.isValidSymbol(symbol)) {
                             System.out.println("Invalid ticket symbol - Try again");
+                            errorLogger.warn("Invalid ticket symbol - Try again");
                             break;
                         }
                         quoteService.displayOne(symbol);
                         break;
                     case 4:
+                        flowLogger.info("Exiting...");
                         System.out.println("Exiting...");
                         scanner.close();
                         System.exit(0);
                     default:
                         System.out.println("Invalid choice. Please enter a number from 1-4");
+                        errorLogger.warn("Invalid choice. Please enter a number from 1-4");
                 }
             } catch (InputMismatchException e) {
+                errorLogger.warn("Invalid input. Please enter a valid number.");
                 System.out.flush();
                 System.out.println("Invalid input. Please enter a valid number.");
                 scanner.next();
